cfe6ec6e7f095d92a013e9e999a80db882b50b77
Kelly Zhang <kellyzhang324@gmail.com>
2023-11-30 23:03:48 -0600

    six can run, walk, rotate

--- Final_Project_Nightmares/Assets/Scripts/PlayerController.cs
+++ Final_Project_Nightmares/Assets/Scripts/PlayerController.cs
@@ -7,24 +7,48 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private PlayerInput playerInput;
     [SerializeField] private Rigidbody rb;
+    private Animator animator;
     private InputAction moveAction;
+    private InputAction runAction;
     public float speed;
+    public float rotateSpeed = 2f;
+    private float speedMultiplier = 1f;
 
+    //private Quaternion lastRotation = Quaternion.identity;
     //public float jumpVelocity;
     // six  can idle, walk, run, push, crouchwalk, climb wall
     void Start()
     {
+        animator = GetComponentInChildren<Animator>();
         moveAction = playerInput.actions["NewMove"];
+        runAction = playerInput.actions["Run"];
     }
 
     private void OnEnable()
     {
-        moveAction.Enable();
+        if (moveAction!= null)
+        {
+            moveAction.Enable();
+        }
+
+        if (runAction != null)
+        {
+            runAction.Enable();
+        }
+
     }
 
     private void OnDisable()
     {
-        moveAction.Disable();
+        if (moveAction != null)
+        {
+            moveAction.Disable();
+        }
+
+        if (runAction != null)
+        {
+            runAction.Disable();
+        }
     }
 
     void Update()
@@ -37,20 +61,31 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 moveInput = moveAction.ReadValue<Vector3>();
 
+        if (moveInput != Vector3.zero)
+        {
+            // get angle and rotation
+            float angle = Mathf.Atan2(moveInput.x, moveInput.z) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(0, angle, 0);
 
+            //rotate in direction of movement
+            rb.MoveRotation(Quaternion.Slerp(rb.rotation, targetRotation, rotateSpeed * Time.deltaTime));
+            //Debug.Log(angle + " " + Quaternion.Euler(0, angle, 0));
 
-        // move left and right
-        Vector3 moveDir = new Vector3(moveInput.x * speed, 0f, moveInput.z * speed);
-        rb.AddForce(moveDir);
+            // move left and right
+            Vector3 moveDir = new Vector3(moveInput.x * speed * speedMultiplier, 0f, moveInput.z * speed * speedMultiplier);
+            rb.AddForce(moveDir);
+            if (animator != null) animator.SetFloat("speed", speed * speedMultiplier);
 
-        //jump
-        rb.AddForce(new Vector3(0f, moveInput.y), ForceMode.Impulse);
-
-        //Quaternion faceDirMovement =
-        rb.MoveRotation(moveDir);
-
+            //jump
+            rb.AddForce(new Vector3(0f, moveInput.y), ForceMode.Impulse);
+        }
+        
         // todo: prevent double-jumping
     }
 
-
+    public void OnRun(InputAction.CallbackContext aContext)
+    {
+        speedMultiplier = 2f;
+        if (animator != null) animator.SetBool("IsRuning", aContext.performed);
+    }
 }
