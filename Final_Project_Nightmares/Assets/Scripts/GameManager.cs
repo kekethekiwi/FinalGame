619a143729cdb0e120699165bece6d64a3070342
Kelly Zhang <kellyzhang324@gmail.com>
2023-12-02 02:18:33 -0600

    troubleshooting game data

--- Final_Project_Nightmares/Assets/Scripts/GameManager.cs
+++ Final_Project_Nightmares/Assets/Scripts/GameManager.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public TMP_Text displaySaveSlot;
+    public static GameManager gameManager;
+    private static int saveSlot;
+
+    private void Awake()
+    {
+        if (gameManager != null) Destroy(this.gameObject);
+        gameManager = this;
+        saveSlot = SaveManager.GetSaveSlot();
+        SaveManager.LoadGame();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SaveManager.SetSaveSlot(1);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveManager.SaveGame();
     }
 }
---
+++ Final_Project_Nightmares/Assets/Scripts/SaveManager.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SaveManager : MonoBehaviour
+{
+    public static SaveManager saveManager;
+    private static GameData currentData;
+    private static string mainFilePath;
+
+    private void Awake()
+    {
+        if (saveManager != null) Destroy(this.gameObject);
+        saveManager = this;
+        mainFilePath = Path.Combine(Application.persistentDataPath, "save.json");
+        LoadGame();
+    }
+
+    public static void SetSaveSlot(int slot)
+    {
+        currentData.saveSlot = Mathf.Max(slot, 1);
+    }
+    public static int GetSaveSlot()
+    {
+        return currentData.saveSlot;
+    }
+
+    public static void SaveGame()
+    {
+        SaveData(currentData, mainFilePath);
+    }
+
+    private static void SaveData(GameData aData, string aPath)
+    {
+        try
+        {
+            string saveData = JsonUtility.ToJson(aData);
+            File.WriteAllText(saveData, aPath);
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError(e);
+        }
+
+        //try
+        //{
+        //    using (StreamWriter writer = new StreamWriter(aPath))
+        //    {
+        //        string dataToWrite = JsonUtility.ToJson(aData);
+        //        writer.Write(dataToWrite);
+        //    }
+        //}
+        //catch(System.Exception e)
+        //{
+        //    Debug.LogError(e);
+        //}
+
+    }   
+
+    public static void LoadGame()
+    {
+        GameData aData = LoadData(mainFilePath);
+        if (aData != null)
+        {
+            currentData = aData;
+        }
+        else
+        {
+            currentData = new GameData();
+        }
+    }
+
+    private static GameData LoadData(string aPath)
+    {
+        try
+        {
+            string saveData = File.ReadAllText(aPath);
+            return JsonUtility.FromJson<GameData>(saveData);
+            
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError(e);
+        }
+        return null;
+    }
+}
+
+[System.Serializable]
+public class GameData
+{
+    public int saveSlot = 1;
+}
