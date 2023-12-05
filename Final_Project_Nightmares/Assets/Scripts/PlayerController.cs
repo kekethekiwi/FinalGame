c90356d87ac60200d5c4819e386e273292aef7cd
Kelly Zhang <kellyzhang324@gmail.com>
2023-12-05 12:51:17 -0600

    fixed floating, meshes, sliding

--- Final_Project_Nightmares/Assets/Scripts/PlayerController.cs
+++ Final_Project_Nightmares/Assets/Scripts/PlayerController.cs
@@ -23,8 +23,9 @@ public class PlayerController : MonoBehaviour
     private float climbableLookAtAngle;
     private bool isClimbing = false;
     private state currentState = state.idle;
+    private Quaternion lastRotation = Quaternion.identity;
 
-    //private Quaternion lastRotation = Quaternion.identity;
+    private Vector3 lastPosition;
     //public float jumpVelocity;
     // six  can *idle, *jump, *walk, run, push, crouchwalk, climb wall
     void Start()
@@ -32,7 +33,8 @@ public class PlayerController : MonoBehaviour
         animator = GetComponentInChildren<Animator>();
         moveAction = playerInput.actions["NewMove"];
         runAction = playerInput.actions["Run"];
-    }
+        lastPosition = rb.position;
+}
 
     private void OnEnable()
     {
@@ -72,88 +74,6 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        // climb
-        if (animator != null) animator.SetBool("isClimbing", isClimbing);
-
-        switch (currentState) {
-            case state.idle:
-                {
-                    Vector3 moveInput = moveAction.ReadValue<Vector3>();
-                    if (moveInput != Vector3.zero)
-                    {
-                        rb.freezeRotation = false;
-                        currentState = moveInput.y > 0f ? state.jump:state.move;
-                    }
-                    else
-                    {
-                        if (animator != null) animator.SetFloat("speed", 0f);
-                        rb.freezeRotation = true;
-                    }
-
-                    break;
-                }
-                
-            case state.move:
-                {
-                    Vector3 moveInput = moveAction.ReadValue<Vector3>();
-                    if (moveInput == Vector3.zero)
-                    {
-                        currentState = state.idle;
-                    }
-                    else
-                    {
-                        // get angle and rotation
-                        float angle = Mathf.Atan2(moveInput.x, moveInput.z) * Mathf.Rad2Deg;
-                        Quaternion targetRotation = Quaternion.Euler(0, angle, 0);
-
-                        //rotate in direction of movement
-                        rb.MoveRotation(Quaternion.Slerp(rb.rotation, targetRotation, rotateSpeed * Time.deltaTime));
-
-                        // move left and right
-                        Vector3 moveDir = new Vector3(moveInput.x * speed * speedMultiplier, 0f, moveInput.z * speed * speedMultiplier);
-                        rb.AddForce(moveDir);
-                        if (animator != null) animator.SetFloat("speed", speed * speedMultiplier);
-
-                        if (moveInput.y > 0f) currentState = state.jump;
-                    }
-                    break;
-                }
-            case state.jump:
-                {
-                    // todo: prevent double-jumping
-                    Vector3 moveInput = moveAction.ReadValue<Vector3>();
-                    if (moveInput.y > 0f)
-                    {
-                        rb.AddForce(new Vector3(0f, moveInput.y), ForceMode.Impulse);
-                        if (animator != null) animator.SetTrigger("jump");
-                        GameManager.ShakeTheCamera(.03f, .03f);
-                    }
-                    else if (moveInput.x > 0f || moveInput.y > 0f)
-                    {
-                        currentState = state.move;
-                        
-                    }
-                    else
-                    {
-                        currentState = state.idle;
-                    }
-                    //if (animator != null) animator.ResetTrigger("jump");
-                    break;
-                }
-            case state.climb:
-                {
-                    if (!Input.GetKeyDown(KeyCode.H))
-                    {
-                        currentState = state.idle;
-                    }
-
-                    // maybe make gravity unchecked
-                    break;
-                }
-
-
-        }
-
         // IGNORE Test Code
         //transform.Translate(moveInput.x * speed * Time.deltaTime, 0f, moveInput.y * speed * Time.deltaTime, Space.World);
     }
@@ -207,13 +127,14 @@ public class PlayerController : MonoBehaviour
                     Vector3 moveInput = moveAction.ReadValue<Vector3>();
                     if (moveInput != Vector3.zero)
                     {
-                        rb.freezeRotation = false;
+                        //rb.freezeRotation = false;
                         currentState = moveInput.y > 0f ? state.jump : state.move;
                     }
                     else
                     {
                         if (animator != null) animator.SetFloat("speed", 0f);
-                        rb.freezeRotation = true;
+                        rb.rotation = lastRotation;
+                        rb.position = lastPosition;
                     }
 
                     break;
@@ -221,9 +142,10 @@ public class PlayerController : MonoBehaviour
 
             case state.move:
                 {
-                    Vector3 moveInput = moveAction.ReadValue<Vector3>();
+                    Vector3 moveInput = moveAction.ReadValue<Vector3>().normalized;
                     if (moveInput == Vector3.zero)
                     {
+                        lastPosition = rb.position;
                         currentState = state.idle;
                     }
                     else
@@ -234,15 +156,18 @@ public class PlayerController : MonoBehaviour
 
                         //rotate in direction of movement
                         rb.MoveRotation(Quaternion.Slerp(rb.rotation, targetRotation, rotateSpeed * Time.deltaTime));
+                        lastRotation = rb.rotation;
 
                         // move left and right
                         Vector3 moveDir = new Vector3(moveInput.x * speed * speedMultiplier, 0f, moveInput.z * speed * speedMultiplier);
                         rb.AddForce(moveDir);
+                        lastPosition = rb.position;
                         if (animator != null) animator.SetFloat("speed", speed * speedMultiplier);
 
                         // pfx
                         PlayPFX(dustPFX);
 
+                        // switch to jump
                         if (moveInput.y > 0f) currentState = state.jump;
                     }
                     break;
@@ -254,8 +179,9 @@ public class PlayerController : MonoBehaviour
                     if (moveInput.y > 0f)
                     {
                         rb.AddForce(new Vector3(0f, moveInput.y), ForceMode.Impulse);
+                        lastPosition = rb.position;
                         if (animator != null) animator.SetTrigger("jump");
-                        GameManager.ShakeTheCamera(.03f, .03f);
+                        //GameManager.ShakeTheCamera(.03f, .03f);
                     }
                     else if (moveInput.x > 0f || moveInput.y > 0f)
                     {
@@ -299,6 +225,7 @@ public class PlayerController : MonoBehaviour
             Debug.Log("I'm climbing wall");
             currentState = state.climb;
             rb.AddForce(moveInput.x * speed, 200f, moveInput.z * speed);
+            lastPosition = rb.position;
             isClimbing = true;
         }
         isClimbing = false;
