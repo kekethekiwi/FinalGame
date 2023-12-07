b060500e6ee778b2f77b229ce0d898d0a0be92db
Kelly Zhang <kellyzhang324@gmail.com>
2023-12-07 01:06:10 -0600

    stopping point today, WIP audio

---
+++ Final_Project_Nightmares/Assets/Scripts/MusicTrackScriptable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTrackScriptable : ScriptableObject
+{
+    public AudioClip mainMusicClip;
+    public bool loops = true;
+}
